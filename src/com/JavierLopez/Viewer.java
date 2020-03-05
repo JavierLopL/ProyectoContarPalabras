31d24133887cd4ec65ef9c0e881040aca41c8bb2
Javier López <javier.lopez14@educa.madrid.org>
2020-03-05 10:42:01 +0100

    Creada documentación de la clase Viewer.java

--- src/com/JavierLopez/Viewer.java
+++ src/com/JavierLopez/Viewer.java
@@ -2,8 +2,19 @@ package com.JavierLopez;
 
 import java.util.HashMap;
 
+/**
+ * @author Javier Lopez Lumbreras
+ * @version 0.3
+ * 
+ *          Clase que muestra al usuario los menús y mensaje en pantalla, junto
+ *          con los resultados de los diferentes análisis de la cadena de texto.
+ *
+ */
 public class Viewer {
 
+	/**
+	 * Muestra al usuario un menú de opciones.
+	 */
 	public void showMenu() {
 		System.out.println("Introduce un número para seleccionar una opción: ");
 		System.out.println("[1] Contar el número total de caracteres");
@@ -14,14 +25,32 @@ public class Viewer {
 		System.out.println("[6] Salir del programa");
 	}
 
+	/**
+	 * Muestra el número total de caracteres de la cadena de texto.
+	 * 
+	 * @param cadena Cadena de texto original introducida por el usuario.
+	 * @param total  El número total de caracteres de la cadena.
+	 */
 	public void textoCaracteresTotal(String cadena, int total) {
 		System.out.println("El número de caracteres en el texto '" + cadena + "' es: " + total + "\n");
 	}
 
+	/**
+	 * Muetsra el número de palabras de la cadena de texto.
+	 * 
+	 * @param cadena Cadena de texto original introducida por el usuario.
+	 * @param total  El número total de palabras de la cadena.
+	 */
 	public void textoNumeroPalabras(String cadena, int total) {
 		System.out.println("El número de palabras en el texto '" + cadena + "' es: " + total + "\n");
 	}
 
+	/**
+	 * @param cadena Cadena de texto original introducida por el usuario.
+	 * @param map    HashMap <Character, Integer> que como clave contiene los
+	 *               caracteres de la cadena y como valor el número de veces que se
+	 *               repite ese carácter.
+	 */
 	public void textoTodosCaracteres(String cadena, HashMap<Character, Integer> map) {
 		String resultado = "";
 		for (Character key : map.keySet()) {
@@ -30,19 +59,31 @@ public class Viewer {
 		System.out.println("La recurrencia de caracteres en el texto '" + cadena + "' es: \n" + resultado);
 	}
 
+	/**
+	 * Muestra un mensaje de bienevenida al iniciar el programa.
+	 */
 	public void start() {
 		System.out.println("¡Bienvenido a String Analyzer 0.3! \n");
 	}
 
+	/**
+	 * Muestra un mensaje indicando al usuario que debe introducir una cadena de texto.
+	 */
 	public void askForString() {
 		System.out.println("Introduce una cadena de texto para analizar:");
 		System.out.print(">");
 	}
 
+	/**
+	 * Muestra un mensaje de error cuando el usuario no introduce una opción válida en el menú del programa.
+	 */
 	public void showErrorMessage() {
 		System.out.println("Lo siento, no he podido entenderte.");
 	}
 
+	/**
+	 * Muestra un mensaje de despedida al cerrar el programa.
+	 */
 	public void showExitMessage() {
 		System.out.println("¡Adiós!");
 	}
